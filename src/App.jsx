a7e0a77c62472687d140cdad52dc41707e8263cb
SKele10 <shoaib.sk.10.sk@gmail.com>
2024-12-11 20:22:10 -0500

    Final version

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import Highlights from "./components/Highlights.jsx";
 import Hero from "./components/Hero.jsx";
 import Navbar from "./components/Navbar.jsx";
+import Model from "./components/Model.jsx";
+import Features from "./components/Features.jsx";
+import Chip from "./components/Chip.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
   return (
@@ -8,6 +12,10 @@ const App = () => {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
+      <Features />
+      <Chip />
+      <Footer />
     </main>
   );
 };
